d92fb9834d62301533703d0e8be260c274516588
unknown <Administrator@GTA-20140101OKD.gtadata.com>
2015-09-07 16:31:42 +0800

    toolbar

--- app/src/main/java/com/wooi/vibox/demo/MainActivity.java
+++ app/src/main/java/com/wooi/vibox/demo/MainActivity.java
@@ -19,11 +19,10 @@ package com.wooi.vibox.demo;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Toast;
 
 import com.sina.weibo.sdk.utils.LogUtil;
 import com.wooi.vibox.R;
@@ -36,7 +35,7 @@ import com.wooi.vibox.demo.openapi.WBOpenAPIActivity;
  * @author wooi
  * @since 2015-8-21
  */
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
 
     /**
      * @see {@link Activity#onCreate}
@@ -48,38 +47,26 @@ public class MainActivity extends Activity {
         LogUtil.sIsLogEnable = true;
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.inflateMenu(R.menu.menu_main);
-        toolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                switch (item.getItemId()){
-                    case R.id.action_settings:
-                        Toast.makeText(getApplicationContext(),"Setting",Toast.LENGTH_LONG).show();
-                        break;
-                }
+        setSupportActionBar(toolbar);
 
-                return true;
-            }
-        });
-        // 微博授权功能
-        this.findViewById(R.id.feature_oauth).setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-//                startActivity(new Intent(MainActivity.this, WBAuthActivity.class));
-                startActivity(new Intent(MainActivity.this, WBAuthActivity.class));
-            }
-        });
-
-        // 开放接口（Open API）功能
-        this.findViewById(R.id.feature_open_api).setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, WBOpenAPIActivity.class));
-            }
-        });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
-
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.demo:
+                startActivity(new Intent(MainActivity.this, WBAuthActivity.class));
+                break;
+            case R.id.open_api:
+                startActivity(new Intent(MainActivity.this, WBOpenAPIActivity.class));
+                break;
+        }
+        return true;
+    }
 }
