c6a6162cce2f8245064afdf22595710efe1fd4d8
wooi <sync6288@gmail.com>
2015-10-03 02:20:06 +0800

    comments

--- app/src/main/java/com/wooi/vibox/adapter/CommentsRVAdapter.java
+++ app/src/main/java/com/wooi/vibox/adapter/CommentsRVAdapter.java
@@ -5,6 +5,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.sina.weibo.sdk.openapi.models.Comment;
 import com.wooi.vibox.R;
@@ -12,28 +13,37 @@ import com.wooi.vibox.model.Comments;
 
 import java.util.ArrayList;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * Created by Wooi on 15/10/2.
  */
 public class CommentsRVAdapter extends RecyclerView.Adapter<CommentsRVAdapter.ViewHolder> {
-    private ArrayList<Comment> commentsList;
+    private ArrayList<Comments> commentsList;
     private Context context;
 
-    public CommentsRVAdapter(Context context,ArrayList<Comment> commentsList) {
+    public CommentsRVAdapter(Context context, ArrayList<Comments> commentsList) {
         this.context = context;
         this.commentsList = commentsList;
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(context).inflate(R.layout.comments_item,parent,false);
+        View view = LayoutInflater.from(context).inflate(R.layout.comments_item, parent, false);
         ViewHolder viewHolder = new ViewHolder(view);
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        Comments comments = commentsList.get(position);
+        holder.commentTv.setText(comments.getText());
+        if(comments.getReply_comment()!=null){
+            holder.replyCommentTv.setText(comments.getReply_comment().getText());
+        }else{
+            holder.replyCommentTv.setText(comments.getStatus().getText());
+        }
     }
 
     @Override
@@ -41,9 +51,24 @@ public class CommentsRVAdapter extends RecyclerView.Adapter<CommentsRVAdapter.Vi
         return commentsList.size();
     }
 
+    /**
+     * This class contains all butterknife-injected Views & Layouts from layout file 'comments_item.xml'
+     * for easy to all layout elements.
+     *
+     * @author ButterKnifeZelezny, plugin for Android Studio by Avast Developers (http://github.com/avast)
+     */
+    static
+
     public class ViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.comment_tv)
+        TextView commentTv;
+        @Bind(R.id.reply_comment_tv)
+        TextView replyCommentTv;
         public ViewHolder(View itemView) {
             super(itemView);
+            ButterKnife.bind(this, itemView);
         }
     }
+
 }
--- app/src/main/java/com/wooi/vibox/fragment/CommentsByMeFragment.java
+++ app/src/main/java/com/wooi/vibox/fragment/CommentsByMeFragment.java
@@ -1,14 +1,19 @@
 package com.wooi.vibox.fragment;
 
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 import com.wooi.vibox.R;
-import com.wooi.vibox.fragment.BaseFragment;
+import com.wooi.vibox.adapter.CommentsRVAdapter;
+import com.wooi.vibox.model.Comments;
 import com.wooi.vibox.util.Content;
 import com.wooi.vibox.util.GetJSONArray;
 import com.wooi.vibox.util.HttpUtil;
@@ -17,15 +22,28 @@ import org.apache.http.Header;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 /**
  * Created by Wooi on 15/10/1.
  */
 public class CommentsByMeFragment extends BaseFragment {
 
+    @Bind(R.id.comments_rv)
+    RecyclerView commentsRv;
+
     @Override
     protected View initView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.comments_fragment_layout,container,false);
-        return view ;
+        View view = inflater.inflate(R.layout.comments_fragment_layout, container, false);
+        ButterKnife.bind(this, view);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(mContext);
+        commentsRv.setHasFixedSize(true);
+        commentsRv.setLayoutManager(layoutManager);
+        return view;
     }
 
     @Override
@@ -34,14 +52,33 @@ public class CommentsByMeFragment extends BaseFragment {
     }
 
     public void getComments() {
-        HttpUtil.get(Content.COMMENTSBYME,new RequestParams(),new JsonHttpResponseHandler(){
+        HttpUtil.get(Content.COMMENTSBYME, new RequestParams(), new JsonHttpResponseHandler() {
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 super.onSuccess(statusCode, headers, response);
                 JSONArray byMeJSONArray = GetJSONArray.getComments(response);
+                ArrayList<Comments> commentsList = getComment(byMeJSONArray);
+                setViewAdapter(commentsList);
             }
         });
     }
 
+    private ArrayList<Comments> getComment(JSONArray byMeJSONArray) {
+        Type typelist = new TypeToken<ArrayList<Comments>>() {
+        }.getType();
+        return new Gson().fromJson(byMeJSONArray.toString(), typelist);
 
+    }
+
+    private void setViewAdapter(ArrayList<Comments> commentsList) {
+        CommentsRVAdapter adapter = new CommentsRVAdapter(mContext, commentsList);
+        commentsRv.setAdapter(adapter);
+    }
+
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
 }
